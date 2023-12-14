3f14e6d9e184e9cf6b3c4648e229d0c233e90baf
Blessing <31315580+blessingsunday1995@users.noreply.github.com>
2023-12-14 10:45:09 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0gER5aTt2O0RKCxH3TycgD3pTGSTBAX8",
+  authDomain: "shop-2dde4.firebaseapp.com",
+  projectId: "shop-2dde4",
+  storageBucket: "shop-2dde4.appspot.com",
+  messagingSenderId: "166389717708",
+  appId: "1:166389717708:web:788c4eef52daf488a1c069"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/requestMethods.js
@@ -0,0 +1,39 @@
+// import axios from "axios";
+
+// const BASE_URL = "http://localhost:5000/api/";
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+
+// export const publicRequest = axios.create({
+//   baseURL: BASE_URL,
+// });
+
+// export const userRequest = axios.create({
+//   baseURL: BASE_URL,
+//   headers: { token: `Bearer ${TOKEN}` },
+// });
+
+
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/";
+
+// Check if localStorage item exists and has the expected structure
+// const persistedData = JSON.parse(localStorage.getItem("persist:root"));
+// const user = persistedData?.user ? JSON.parse(persistedData.user) : null;
+// const TOKEN = user?.currentUser?.accessToken || null;
+
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
+
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: { authorization: `Bearer ${TOKEN}` },
+});
